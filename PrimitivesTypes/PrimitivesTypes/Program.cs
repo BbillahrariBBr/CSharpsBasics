ae9f073cf9b5398201c2a017281d2f2765d8e80a
BbillahrariBBr <mb.billah8@gmail.com>
2023-03-24 20:22:05 +0600

    start c sharp practice for office

---
+++ PrimitivesTypes/PrimitivesTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitivesTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringVar = "Hello World";
+            int intVar = 100;
+            float floatVar = 100.2f;
+            char  charVar = 'a';
+            bool boolVar = false;
+        }
+    }
+}
