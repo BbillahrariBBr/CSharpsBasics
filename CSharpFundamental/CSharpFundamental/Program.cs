738c03860c92a0831219ccac8db0ce52d1b63c46
BbillahrariBBr <mb.billah8@gmail.com>
2023-03-24 20:55:52 +0600

    non primitives types static 05 done

--- CSharpFundamental/CSharpFundamental/Program.cs
+++ CSharpFundamental/CSharpFundamental/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpFundamental.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,9 @@ namespace CSharpFundamental
             person.firstName = "Baki";
             person.lastName = "Billah";
             person.Introduce();
+
+            var result = Calculator.Add(13, 20);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
