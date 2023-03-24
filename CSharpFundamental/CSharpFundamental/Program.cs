fac131ae6710d3cca9fb2acac5bc3647ae6402a2
BbillahrariBBr <mb.billah8@gmail.com>
2023-03-24 20:43:43 +0600

    non primitives types 04 done

---
+++ CSharpFundamental/CSharpFundamental/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamental
+{
+    public class Person {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce() {
+            Console.WriteLine("My Name is " + firstName + " " + lastName);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Baki";
+            person.lastName = "Billah";
+            person.Introduce();
+            Console.ReadLine();
+        }
+    }
+}
