e2547e20d3c2e47ded4a432e6aad21e08fbea8b7
BbillahrariBBr <mb.billah8@gmail.com>
2023-03-24 21:05:48 +0600

    string 06 done

--- CSharpFundamental/CSharpFundamental/Program.cs
+++ CSharpFundamental/CSharpFundamental/Program.cs
@@ -19,14 +19,25 @@ namespace CSharpFundamental
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
+            // class 
+            /* Person person = new Person();
             person.firstName = "Baki";
             person.lastName = "Billah";
             person.Introduce();
 
             var result = Calculator.Add(13, 20);
             Console.WriteLine(result);
+            Console.ReadLine(); */
+
+            // array
+            // string 
+            string firstName = "Baki";
+            string lastName = "Billah";
+            string fullName = string.Concat(firstName, lastName);
+            Console.WriteLine(fullName);
+            Console.WriteLine(firstName.IndexOf('k'));
             Console.ReadLine();
+
         }
     }
 }
