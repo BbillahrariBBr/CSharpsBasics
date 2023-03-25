7e4af0608ebffcb12040948e6318557ad1ed399b
BbillahrariBBr <mb.billah8@gmail.com>
2023-03-25 19:25:58 +0600

    inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public string name;
+        public int age;
+        public string color;
+
+        public void printDetails()
+        { Console.WriteLine("Name: " + name + " Age: " + age + " Color: " + color); }
+    }
+
+    class Dog : Animal {
+        public string bred;
+
+        public void sound()
+        {
+            Console.WriteLine("Dog Sound: Ghew Ghew");
+        }
+
+        public Dog(string _name, int _age, string _color, string _bred) {
+            name = _name;  
+            age = _age; 
+            color = _color; 
+            bred = _bred;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Audi", 5, "grey", "bangla");
+
+            dog.printDetails();
+            dog.sound();
+
+        }
+    }
+}
