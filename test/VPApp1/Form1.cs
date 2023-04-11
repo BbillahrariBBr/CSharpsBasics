6a95998ca92b37eb55de18513df12ced2197b8ea
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-11 20:10:16 +0600

    windows Form button clicked and text change

---
+++ test/VPApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPApp1
+{
+    public partial class frmHomePage : Form
+    {
+        public frmHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHomePage_Load(object sender, EventArgs e)
+        {
+            lblHello.Text = "Hello World";
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            lblHello.Text = "Hello World Again Again ...";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            lblHello.Text = "Text has been canceld";
+        }
+    }
+}
