11709d77431c29a3aa200143500b9658fc112567
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-15 21:22:50 +0600

    signUp frm design

---
+++ test/VpApp2/frmSignUp.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace VpApp2
+{
+    partial class frmSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSmsMsg = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxFirstName = new System.Windows.Forms.TextBox();
+            this.tbxLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chbxSms = new System.Windows.Forms.CheckBox();
+            this.chbxReports = new System.Windows.Forms.CheckBox();
+            this.chbxTransection = new System.Windows.Forms.CheckBox();
+            this.lblTransMsg = new System.Windows.Forms.Label();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSmsMsg
+            // 
+            this.lblSmsMsg.AutoSize = true;
+            this.lblSmsMsg.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSmsMsg.Location = new System.Drawing.Point(226, 40);
+            this.lblSmsMsg.Name = "lblSmsMsg";
+            this.lblSmsMsg.Size = new System.Drawing.Size(44, 18);
+            this.lblSmsMsg.TabIndex = 0;
+            this.lblSmsMsg.Text = ".........";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Name";
+            // 
+            // tbxFirstName
+            // 
+            this.tbxFirstName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxFirstName.Location = new System.Drawing.Point(216, 51);
+            this.tbxFirstName.Name = "tbxFirstName";
+            this.tbxFirstName.Size = new System.Drawing.Size(410, 29);
+            this.tbxFirstName.TabIndex = 2;
+            // 
+            // tbxLastName
+            // 
+            this.tbxLastName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxLastName.Location = new System.Drawing.Point(216, 104);
+            this.tbxLastName.Name = "tbxLastName";
+            this.tbxLastName.Size = new System.Drawing.Size(410, 29);
+            this.tbxLastName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name";
+            // 
+            // tbxEmail
+            // 
+            this.tbxEmail.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxEmail.Location = new System.Drawing.Point(216, 159);
+            this.tbxEmail.Name = "tbxEmail";
+            this.tbxEmail.Size = new System.Drawing.Size(410, 29);
+            this.tbxEmail.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Email";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTransMsg);
+            this.groupBox1.Controls.Add(this.chbxTransection);
+            this.groupBox1.Controls.Add(this.chbxReports);
+            this.groupBox1.Controls.Add(this.chbxSms);
+            this.groupBox1.Controls.Add(this.lblSmsMsg);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(84, 272);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 171);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Services";
+            // 
+            // chbxSms
+            // 
+            this.chbxSms.AutoSize = true;
+            this.chbxSms.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbxSms.Location = new System.Drawing.Point(35, 40);
+            this.chbxSms.Name = "chbxSms";
+            this.chbxSms.Size = new System.Drawing.Size(153, 23);
+            this.chbxSms.TabIndex = 0;
+            this.chbxSms.Text = "Sms Notification";
+            this.chbxSms.UseVisualStyleBackColor = true;
+            // 
+            // chbxReports
+            // 
+            this.chbxReports.AutoSize = true;
+            this.chbxReports.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbxReports.Location = new System.Drawing.Point(35, 87);
+            this.chbxReports.Name = "chbxReports";
+            this.chbxReports.Size = new System.Drawing.Size(89, 23);
+            this.chbxReports.TabIndex = 1;
+            this.chbxReports.Text = "Reports";
+            this.chbxReports.UseVisualStyleBackColor = true;
+            // 
+            // chbxTransection
+            // 
+            this.chbxTransection.AutoSize = true;
+            this.chbxTransection.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbxTransection.Location = new System.Drawing.Point(35, 131);
+            this.chbxTransection.Name = "chbxTransection";
+            this.chbxTransection.Size = new System.Drawing.Size(183, 23);
+            this.chbxTransection.TabIndex = 2;
+            this.chbxTransection.Text = "Transection Reports";
+            this.chbxTransection.UseVisualStyleBackColor = true;
+            // 
+            // lblTransMsg
+            // 
+            this.lblTransMsg.AutoSize = true;
+            this.lblTransMsg.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTransMsg.Location = new System.Drawing.Point(226, 136);
+            this.lblTransMsg.Name = "lblTransMsg";
+            this.lblTransMsg.Size = new System.Drawing.Size(44, 18);
+            this.lblTransMsg.TabIndex = 3;
+            this.lblTransMsg.Text = ".........";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignUp.Location = new System.Drawing.Point(525, 475);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(101, 36);
+            this.btnSignUp.TabIndex = 8;
+            this.btnSignUp.Text = "SignUp";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            // 
+            // frmSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 536);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbxEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxFirstName);
+            this.Controls.Add(this.label2);
+            this.Name = "frmSignUp";
+            this.Text = "SignUp Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSmsMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxFirstName;
+        private System.Windows.Forms.TextBox tbxLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTransMsg;
+        private System.Windows.Forms.CheckBox chbxTransection;
+        private System.Windows.Forms.CheckBox chbxReports;
+        private System.Windows.Forms.CheckBox chbxSms;
+        private System.Windows.Forms.Button btnSignUp;
+    }
+}
+
