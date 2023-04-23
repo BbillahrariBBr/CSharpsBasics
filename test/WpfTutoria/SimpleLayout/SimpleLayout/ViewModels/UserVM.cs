93ae7ae9a7306a5c5b0d76508c4db7c0e0c48619
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-23 23:06:46 +0600

    vm add

---
+++ test/WpfTutoria/SimpleLayout/SimpleLayout/ViewModels/UserVM.cs
@@ -0,0 +1,44 @@
+﻿using SimpleLayout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLayout.ViewModels
+{
+
+    internal class UserVM : ViewModelBase
+    {
+        private User user;
+        private string userName;
+
+        public UserVM()
+        {
+            user = new User();
+
+        }
+
+        public string UserName
+        {
+            get => user.Username;
+            set
+            {
+                user.Username = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        public string Password
+        {
+            get => user.Password;
+            set
+            {
+                user.Password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        
+    }
+}
---
+++ test/WpfTutoria/SimpleLayout/SimpleLayout/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace SimpleLayout.ViewModels
+{
+    public class ViewModelBase: INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        public void OnPropertyChanged(string propertyName)
+            => PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+
+    }
+}
