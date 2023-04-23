09e2df9451a52fcad98432484085744ed86366b9
BbillahrariBBr <mb.billah8@gmail.com>
2023-04-23 18:23:40 +0600

    icon pack add

--- test/WpfTutoria/SimpleLayout/SimpleLayout/obj/Debug/net6.0-windows/App.g.i.cs
+++ test/WpfTutoria/SimpleLayout/SimpleLayout/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace SimpleLayout {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/SimpleLayout;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SimpleLayout;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- test/WpfTutoria/SimpleLayout/SimpleLayout/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ test/WpfTutoria/SimpleLayout/SimpleLayout/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace SimpleLayout {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SimpleLayout;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SimpleLayout;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
